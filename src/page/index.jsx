2081df431c604481145b2da22422363b37b34129
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-06-25 18:11:51 -0300

    feature :sparkles: created components of home and about, installed swipeable-views

---
+++ src/page/index.jsx
@@ -0,0 +1,15 @@
+import About from "../components/About";
+import Header from "../components/Header";
+import Home from "../components/Home";
+
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+    </>
+  );
+};
+
+export default Page;
